cf733226539dba1c1ee1746f9c5837ee7dcd1beb
alohse <alohse3@gatech.edu>
2014-10-21 10:44:58 -0400

    Create PE_perfect_xtal.m

---
+++ CODES/PE_perfect_xtal.m
@@ -0,0 +1,71 @@
+%This code develops perfect crystalline structure based on polyethylene
+%information gathered from the Handbook of Polyethylene.
+
+%For reference: PE forms a BC orthrhombic crystal structure
+
+bond_angle = deg2rad(112);
+bond_length = 0.153; %in nanometers
+
+%initial dimensions of cell in simulation (nm)
+xdim = 4.4;
+ydim = 4.4;
+zdim = 11.2;
+
+%create a vector with the initial chain
+%this vector can then be repeated at the unit cell corners to create a
+%complete crystal over the volume
+k = 1;
+for ii = 0:.254:zdim
+    initchain(k,1) = 0; %x location
+    initchain(k,2) = 0; %y location
+    initchain(k,3) = ii; %z location
+    bc_chain(k,1) = .4356;
+    bc_chain(k,2) = .2475;
+    bc_chain(k,3) = ii;
+    k=k+1;
+end
+
+for jj = 0.127:0.254:zdim
+    initchain(k,1) = 0.0646;
+    initchain(k,2) = 0.0561;
+    initchain(k,3) = jj;
+    bc_chain(k,1) = .371;
+    bc_chain(k,2) = .3037;
+    bc_chain(k,3) = ii;
+    k=k+1;
+end
+
+%we now have one complete chain so we can repeat in a and b accordingly
+%a rotated chain will need to be created for the (body-center)
+chains = initchain;
+ctr = 1;
+for ii = 0.742:0.742:xdim
+    newchainx = initchain(:,1)+ctr*0.742;
+    newbc_chainx = bc_chain(:,1)+ctr*.742;
+    chains = vertcat(chains,[newchainx,initchain(:,2),initchain(:,3)],[newbc_chainx,bc_chain(:,2),bc_chain(:,3)]);
+    ctr = ctr + 1;
+end;
+ctr = 1;
+allchains = chains;
+for ii = .495:.495:ydim
+    newchainy = chains(:,2)+ctr*0.495;
+    allchains = vertcat(allchains,[chains(:,1),newchainy,chains(:,3)]);
+    ctr = ctr+1;
+end
+
+for ii = 1:numel(allchains(:,1))
+    atoms(ii) = ii;
+    id(ii) = 1;
+    mol(ii) = 1;
+end
+%fid = fopen('perfect-xtal.txt','w');
+%fprintf(fid,'ITEM: TIMESTEP\n6400000\n');
+%fprintf(fid,'ITEM: NUMBER OF ATOMS\n7999\n');
+%fprintf(fid,'ITEM: BOX BOUNDS\n');
+%box_bounds = [0.0,4.4;0.0,4.4;0.0,11.2];
+%fprintf(fid,'%.6f %.6f\n', box_bounds(:,1), box_bounds(:,2));
+%fprintf(fid,'ITEM: ATOMS id mol type x y z \n');
+%for ii = 1:numel(atoms)
+%    fprintf(fid,'%d %d %d %.6f %.6f %.6f\n',atoms(ii),id(ii),mol(ii),allchains(ii,1),allchains(ii,2),allchains(ii,3));
+%end
+%fclose(fid);
